815eefd0ed716577426910dc7028921368e025fe
erickawu <36214424+erickawu@users.noreply.github.com>
2020-05-26 21:55:52 -0700

    + back button

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	$("#receipt").fadeIn(1600);
+	$("#receipt").fadeIn(1400);
+
+	$("#1").click(function() {
+		$("#receipt").fadeOut(400, function() {
+        	$("#backbutton").fadeIn(1400).addClass('back');
+    	});
+    });
+
+	$("#backbutton").click(function() {
+		$("#backbutton").fadeOut(400, function() {
+        	$("#receipt").fadeIn(1400);
+    	});
+    });
 });
