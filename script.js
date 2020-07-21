9b3f214008aa7f7b2a816ec737db40fd9a3fdef9
erickawu <36214424+erickawu@users.noreply.github.com>
2020-07-20 23:13:15 -0700

    updated placeholder cards/containers for all non-home pages

--- script.js
+++ script.js
@@ -2,14 +2,37 @@ $(document).ready(function() {
 	$("#receipt").fadeIn(1400);
 
 	$("#1").click(function() {
-		$("#receipt").fadeOut(400, function() {
+		$("#wrapper").fadeOut(400, function() {
         	$("#backbutton").fadeIn(1400).addClass('back');
+            $("#favorites").fadeIn(1400).addClass('projcontainer');
     	});
+    });
+
+    $("#2").click(function() {
+        $("#wrapper").fadeOut(400, function() {
+            $("#backbutton").fadeIn(1400).addClass('back');
+            $("#recipes").fadeIn(1400).addClass('projcontainer');
+        });
+    });
+
+    $("#3").click(function() {
+        $("#wrapper").fadeOut(400, function() {
+            $("#backbutton").fadeIn(1400).addClass('back');
+            $("#moments").fadeIn(1400).addClass('projcontainer');
+        });
+    });
+
+    $("#4").click(function() {
+        $("#wrapper").fadeOut(400, function() {
+            $("#backbutton").fadeIn(1400).addClass('back');
+            $("#projects").fadeIn(1400).addClass('projcontainer');
+        });
     });
 
 	$("#backbutton").click(function() {
 		$("#backbutton").fadeOut(400, function() {
-        	$("#receipt").fadeIn(1400);
+        	$("#wrapper").fadeIn(1400);
     	});
+        $(".projcontainer").fadeOut(400)
     });
 });
